47bec82a0cfd9563e5c3319da95994e051e4fd9b
talenaa <talenabarbosa7@gmail.com>
2025-02-04 10:12:09 +0100

    build: finished flight factory

---
+++ database/factories/FlightFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class FlightFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "date" => fake()->date(),
+            "departure" => fake()->country(),
+            "arrival" => fake()->country(),
+            "plane_id" => fake()->randomDigitNot(0),
+            "aviable" => fake()->boolean()
+        ];
+    }
+}

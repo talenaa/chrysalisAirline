d37deca5751e4f69ed920b51d6612de2772c747d
talenaa <talenabarbosa7@gmail.com>
2025-02-04 10:25:17 +0100

    build: added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Airplane;
+use App\Models\Flight;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +21,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Airplane::factory(10)->create();
+
+        Flight::factory(10)->create();
     }
 }

bde249fa30ec3acbb2ee1c4bf7dcf43d3b3f84fa
talenaa <talenabarbosa7@gmail.com>
2025-02-04 12:17:54 +0100

    feat: airplane routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post("/airplanes", [AirplaneController::class, "store"])->name("apiairplanestore");
+Route::get("/airplanes", [AirplaneController::class, "index"])->name("apiairplaneindex");
+Route::get("/airplanes/{id}", [AirplaneController::class, "show"])->name("apiairplaneshow");
+Route::put("/airplanes/{id}", [AirplaneController::class, "update"])->name("apiairplaneupdate");
+Route::delete("/airplanes/{id}", [AirplaneController::class, "destroy"])->name("apiairplanedestroy");

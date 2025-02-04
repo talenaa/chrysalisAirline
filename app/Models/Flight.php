47a9a8eab29783346d4fdb8aa81f03e3db163d62
talenaa <talenabarbosa7@gmail.com>
2025-02-04 10:32:53 +0100

    feat: added flight model

---
+++ app/Models/Flight.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Flight extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "date",
+        "departure",
+        "arrival",
+        "airplane_id",
+        "aviable"
+    ];
+
+    public function airplane(): BelongsTo
+    {
+        return $this->belongsTo(Airplane::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, "flight_user");
+    }
+}

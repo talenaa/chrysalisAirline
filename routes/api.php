44c51a404839e3e3bbd10da79bba5fbd8b37ab81
talenaa <talenabarbosa7@gmail.com>
2025-03-20 11:40:36 +0100

    feat: auth routes

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\FlightController;
 use App\Http\Controllers\Api\AirplaneController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/me', [AuthController::class, 'me'])-> middleware('auth:api')->name('me');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
+});
 
 Route::post("/airplanes", [AirplaneController::class, "store"])->name("apiairplanestore");
 Route::get("/airplanes", [AirplaneController::class, "index"])->name("apiairplaneindex");

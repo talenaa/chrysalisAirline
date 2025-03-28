e250af3325ca44e472eaccf939cee9abc2442e14
talenaa <talenabarbosa7@gmail.com>
2025-03-28 15:04:07 +0100

    test: second airplane test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,9 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use App\Http\Middleware\RoleMiddleware;
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
--- tests/Feature/AirplaneModelTest.php
+++ tests/Feature/AirplaneModelTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\Flight;
 use App\Models\Airplane;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,7 +12,7 @@ class AirplaneModelTest extends TestCase
 {
     use RefreshDatabase;
 
-    
+
     public function test_CheckIfCanCreateAnAirplane()
     {
         $airplane = Airplane::create([
@@ -23,5 +24,14 @@ class AirplaneModelTest extends TestCase
             'name' => 'Boeing 747',
             'seats' => 300
         ]);
+    }
+
+    public function test_CheckIfHasManyFlights()
+    {
+        $airplane = Airplane::factory()->create();
+        $flights = Flight::factory()->count(3)->create(['airplane_id' => $airplane->id]);
+
+        $this->assertCount(3, $airplane->flights);
+        $this->assertTrue($airplane->flights->first() instanceof Flight);
     }
 }
